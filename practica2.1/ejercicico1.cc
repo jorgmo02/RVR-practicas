e6bada87fdf390ad021ae76375a8062c91c8f582
Jorge <jorgmo02@ucm.es>
2021-05-11 01:53:12 +0200

    Ejercicio 2.1.1 - Prueba de compilación
    Creados task.json y launch.json desde la máquina virtual instalada de 0. El programa compila y ejecuta en modo debug (la consola utilizada es la nativa de VS Code).

---
+++ practica2.1/ejercicico1.cc
@@ -0,0 +1,16 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string>
+#include <iostream>
+
+int main(int argc, char* argv)
+{
+    int a = 0;
+    std::cout << a << std::endl;
+
+    std::cin >> a;
+    std::cout << a << std::endl;
+
+    return 0;
+}
