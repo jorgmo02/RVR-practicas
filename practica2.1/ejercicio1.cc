01ec44beaae1c3c5a472fbe3e17ad3aea86bfded
Jorge <jorgmo02@ucm.es>
2021-05-12 20:04:07 +0200

    Ejercicio 1 terminado
    Me ha hecho click el cerebro y ahora entiendo Redes

--- practica2.1/ejercicio1.cc
+++ practica2.1/ejercicio1.cc
@@ -1,16 +1,45 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-#include <string>
+#include <string.h>
 #include <iostream>
 
 int main(int argc, char** argv)
 {
-    int a = 0;
-    std::cout << a << std::endl;
+    addrinfo hints;      // opciones de filtrado
+    addrinfo* res;       // variable para almacenar el resultado de getaddrinfo
 
-    std::cin >> a;
-    std::cout << a << std::endl;
+    memset((void*) &hints, 0, sizeof(addrinfo));
+
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_flags = AF_UNSPEC;
+
+    int resGetAddr = getaddrinfo(argv[1], 0, &hints, &res);
+
+    if(resGetAddr != 0) // ha habido un error
+    {
+        std::cerr << "ERROR: getaddrinfo failed with errorcode: " << resGetAddr << "\n";
+        return -1;
+    }
+
+    for(addrinfo* i = res; i != nullptr; i = i->ai_next)
+    {
+        char host[NI_MAXHOST];
+        char service[NI_MAXSERV];
+
+        int err = getnameinfo(i->ai_addr, i->ai_addrlen, host, NI_MAXHOST,
+                            service, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
+
+        if (err != 0)    //gestion de errores
+        {
+            std::cerr << "ERROR: getnameinfo failed with errorcode: " << err << "\n";
+            return -1;
+        }
+
+        std::cout << host << "      " << i->ai_family << "     " << i->ai_socktype << "\n";
+    }
+
+    freeaddrinfo(res);
 
     return 0;
 }
