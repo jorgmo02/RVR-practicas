33d6865d962f9dbf10710a4cfc21813b32b82162
Jorge <jorgmo02@ucm.es>
2021-05-16 12:56:03 +0200

    Ejercicio 5
    Bastante parecido al 3

---
+++ practica2.1/ejercicio5.cc
@@ -0,0 +1,68 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+#include <iostream>
+#include <unistd.h>
+
+#define BUFFLEN 80
+
+int main(int argc, char** argv)
+{
+    addrinfo hints;      // opciones de filtrado
+    addrinfo* res;       // variable para almacenar el resultado de getaddrinfo
+
+    memset((void*) &hints, 0, sizeof(addrinfo));
+
+    hints.ai_socktype = SOCK_STREAM;        // tcp
+    hints.ai_family = AF_INET;
+
+    int rgetaddr = getaddrinfo(argv[1], argv[2], &hints, &res);
+    if(rgetaddr != 0) {
+        std::cerr << "ERROR: getaddrinfo failed with errorcode: " << rgetaddr << "\n";
+        return -1;
+    }
+
+    int sd = socket(res->ai_family, res->ai_socktype, 0);
+    if(sd == -1) {
+        std::cerr << "ERROR: socket creation failed\n";
+        return -1;
+    }
+
+
+    if (connect(sd, (sockaddr*) res->ai_addr, res->ai_addrlen) != 0)    // el tam maximo de la cola de conexiones pendientes es 10 (man 2 listen)
+    {
+        std::cout << "ERROR: connect failed with errno: " << errno << "\n";
+        return -1;
+    }
+
+    freeaddrinfo(res);
+
+    while(true)
+    {
+        char buffer[BUFFLEN];
+
+        std::cin >> buffer;
+
+        int sendbytes = strlen(buffer);
+        if(strlen(buffer) == 1 && buffer[0] == 'Q') return 1;
+
+        int sent = send(sd, buffer, sendbytes, 0);
+        if(sent == -1) {
+            std::cout << "ERROR: sendto failed\n";
+            return -1;
+        }
+
+        int bytes = recv(sd, (void*) buffer, BUFFLEN-1, 0); 
+        if(bytes == -1) {
+            std::cout << "ERROR: recvfrom failed\n";
+            return -1;
+        }
+
+        std::cout << buffer;
+    }
+    
+    close(sd);
+
+    return 0;
+}
